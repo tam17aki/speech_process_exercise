3ff8e5bd97bbb91c3cb042b2b0818443002f107c
Akira Tamamori <tamamori5917@gmail.com>
2020-11-05 20:31:03 +0900

    Add files via upload

---
+++ WarmUp/librosa_plot_waveform.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+""" 音声情報処理 n本ノック !! """
+
+# MIT License
+
+# Copyright (C) 2020 by Akira TAMAMORI
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Commentary:
+# librosa を用いた波形プロット (やや処理が重たい)
+# 波形読み込みはscipy.ioのwavfileモジュール
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+
+IN_WAVE_FILE = "in.wav"  # モノラル音声（前提）
+
+# 音声データ読み込み (fsがサンプリング周波数、dataは音声データ)
+fs, data = wavfile.read(IN_WAVE_FILE)
+data = data.astype(np.float64)
+
+# プロット枠を確保 (10がヨコのサイズ、4はタテのサイズ)
+plt.figure(figsize=(10, 4))
+
+# 波形の表示
+librosa.display.waveplot(data, fs)
+
+# 余白を少なくする
+plt.tight_layout()
+
+# 画像を画面表示 (必須)
+plt.show()
