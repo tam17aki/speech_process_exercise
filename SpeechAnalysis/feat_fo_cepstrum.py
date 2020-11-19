1a5ca7d72cc87b23ffafaaa865670e16ee285f82
Akira Tamamori <tamamori5917@gmail.com>
2020-11-19 13:29:31 +0900

    Add files via upload

--- SpeechAnalysis/feat_fo_cepstrum.py
+++ SpeechAnalysis/feat_fo_cepstrum.py
@@ -26,91 +26,70 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Commentary:
-# - 音声からケプストラム法により基本周波数を推定する
-# - パワーが最大となる音声フレームを対象に推定
+# - ケプストラム法により基本周波数の「系列」を推定する
+# - 簡易的な有声音の判定と無声音の判定
 
+import librosa
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy.io import wavfile
-import librosa
-import matplotlib.pyplot as plt
 
-IN_WAVE_FILE = "in.wav"         # 分析対象の音声
+IN_WAVE_FILE = "in.wav"  # 分析対象の音声
 
-FRAME_LENGTH = 1024             # フレーム長 (FFTサイズ)
-HOP_LENGTH = 80                 # フレームのシフト長
+FRAME_LENGTH = 1024  # フレーム長 (FFTサイズ)
+HOP_LENGTH = 80  # フレームのシフト長
 FFT_LENGTH = FRAME_LENGTH
 
-MAX_Fo = 200                    # 分析における基本周波数の最大値 (Hz)
-MIN_Fo = 60                     # 分析における基本周波数の最小値 (Hz)
+MAX_Fo = 200  # 分析における基本周波数の最大値 (Hz)
+MIN_Fo = 60  # 分析における基本周波数の最小値 (Hz)
+
+THRESHOLD_dB = -30  # 無声判定のしきい値 in dB
 
 # 音声のロード
 fs, data = wavfile.read(IN_WAVE_FILE)
 data = data.astype(np.float64)
 
-# ケプストラムの最大次数、最小次数
-max_cep_order = int(np.floor(fs / MIN_Fo))
-min_cep_order = int(np.floor(fs / MAX_Fo))
-
 # フレーム化
-frames = librosa.util.frame(data, frame_length=FRAME_LENGTH,
-                            hop_length=HOP_LENGTH).T
+frames = librosa.util.frame(data, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH).T
 
-# パワーが最大のフレーム位置を取得
-max_ind = np.argmax(np.sum(frames * frames, axis=1))
-
-# パワーが最大となるフレームを取り出す
-pow_max_frame = frames[max_ind, :]
+# 各フレームで計算したパワーをもとに有声音のフレームを決定（泥臭い）
+powers = np.sum(frames * frames, axis=1)
+voiced = np.where(10 * np.log(powers / np.max(powers)) > THRESHOLD_dB)
 
 # 窓掛け
 window = scipy.signal.blackman(FFT_LENGTH)
-windowed_frame = pow_max_frame * window
+windowed_frame = frames[voiced] * window
 
 # ケプストラムの計算 (FFT → 絶対値 → 対数 → 逆FFT)
 fft_spec = scipy.fft.rfft(windowed_frame)
 log_amp_spec = np.log(np.abs(fft_spec))
 cepstrum = scipy.fft.irfft(log_amp_spec)
 
+# ケプストラムの最大次数、最小次数
+max_cep_order = int(np.floor(fs / MIN_Fo))
+min_cep_order = int(np.floor(fs / MAX_Fo))
+
 # ピーク位置の検出
-peak_index = np.argmax(cepstrum[min_cep_order: max_cep_order])
+peak_index = np.argmax(cepstrum[:, min_cep_order:max_cep_order], axis=1)
 max_quef = peak_index + min_cep_order
 
-# ケフレンシに変換して基本周波数の推定
+# ケフレンシから変換して基本周波数の推定
 fo = fs / max_quef
-print(f"Fundamental Frequency = {fo:.2f} Hz")
 
-# 波形表示
-fig = plt.figure(figsize=(12, 8))
-time = np.arange(len(windowed_frame)) / fs
-axes = fig.add_subplot(3, 1, 1)
-axes.plot(time, pow_max_frame, label="original")
-axes.plot(time, windowed_frame, label="windowed")
-axes.set_xlabel("Time (sec)")
-axes.set_ylabel("Amplitude")
-axes.set_title("Waveform")
-axes.legend()
-axes.set_xlim(0, np.max(time))
+# 基本周波数の系列：無声音のフレームでは 0 Hzとするため 一様に0で初期化
+fo_seq = np.zeros(frames.shape[0])
 
-# 相対パワー表示
-axes = fig.add_subplot(3, 1, 2)
-freq = fs/2 * np.arange(len(log_amp_spec)) / len(log_amp_spec)
-logpower = 20 * np.log(np.abs(fft_spec) / np.max(np.abs(fft_spec)))
-axes.plot(freq, logpower, label="original")
-axes.set_xlabel("Frequency (Hz)")
-axes.set_ylabel("Log power (dB)")
-axes.set_title("Log spectrum")
-axes.set_xlim(0, np.max(freq))
-axes.set_ylim(np.min(logpower), 0)
+# 有声音のフレームに 推定された基本周波数を格納する
+fo_seq[voiced] = fo
 
-# ケプストラム表示 (対数振幅)
-axes = fig.add_subplot(3, 1, 3)
-quef = np.arange(FFT_LENGTH / 2) / fs
-log_cepstrum = np.log(np.abs(cepstrum))
-axes.plot(quef, log_cepstrum[:len(quef)])
-axes.set_xlabel("Quefrency (sec)")
-axes.set_ylabel("Log amplitude (dB)")
-axes.set_title("Cepstrum")
-axes.set_xlim(0, np.max(quef))
-
+# 基本周波数の系列を表示
+fig = plt.figure(figsize=(12, 4))
+plt.plot(fo_seq)
+plt.xlabel("Frame number")
+plt.ylabel("Frequency (Hz)")
+plt.title("Estimation of fundamental frequency via cepstrum method")
 plt.tight_layout()
+plt.xlim(0, len(fo_seq) - 1)
+
 plt.show()
