de50d44867ab282ed4e9423b1b279fdd836077d2
Akira Tamamori <tamamori5917@gmail.com>
2022-11-05 00:54:08 +0900

    Update synth_gtts_gui.py

--- SpeechSynthesis/synth_gtts_gui.py
+++ SpeechSynthesis/synth_gtts_gui.py
@@ -26,7 +26,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Commentary:
-# gTTSによるテキスト音声合成のサンプルスクリプト
+# gTTSによるテキスト音声合成のサンプルスクリプト with PySimpleGUI
 
 import subprocess
 
