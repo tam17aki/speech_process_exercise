45f60092e2651b7bb3998caf929ba69917ca34c0
Akira Tamamori <tamamori5917@gmail.com>
2022-11-05 00:54:44 +0900

    Update synth_pyopenjtalk_gui.py

--- SpeechSynthesis/synth_pyopenjtalk_gui.py
+++ SpeechSynthesis/synth_pyopenjtalk_gui.py
@@ -26,7 +26,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Commentary:
-# PyOpenJTalkによるテキスト音声合成のサンプルスクリプト
+# PyOpenJTalkによるテキスト音声合成のサンプルスクリプト with PySimpleGUI
 
 import numpy as np
 import pyopenjtalk
