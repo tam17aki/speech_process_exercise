bf01f0e02fb576de0d0738cf36f02f3e2d2c98aa
Akira Tamamori <tamamori5917@gmail.com>
2022-11-05 12:16:42 +0900

    Add files via upload

---
+++ WarmUp/ffmpeg_mp3_to_wav.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+""" 音声情報処理 n本ノック !! """
+
+# MIT License
+
+# Copyright (C) 2022 by Akira TAMAMORI
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Commentary:
+# ffmpeg-pythonによりmp3をwavへエクスポート
+
+import ffmpeg
+
+IN_MP3_FILE = "in.mp3"
+OUT_WAVE_FILE = "out.wav"
+
+stream = ffmpeg.input(IN_MP3_FILE)
+stream = ffmpeg.output(stream, OUT_WAVE_FILE, format="wav")
+stream.run()
